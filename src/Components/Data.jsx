62f17dd0810b9b0bc60dbe7330a19d1f772d046b
ador <rashedulhaque111@gmail.com>
2022-10-01 19:40:50 +0600

    data grid layout update

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -12,14 +12,15 @@ const Data = () => {
             <h1 className='text-3xl text-center font-bold text-green-400 m-4'>Capsules</h1>
             <div className='grid lg:grid-cols-3 sm:grid-cols-1'>
                 {
-                    capsules.map((capsule, status, capsule_id, missions) =>
+                    capsules.map((capsule, status, capsule_id, missions, capsule_serial) =>
                         <div className="card w-96 bg-blue-400 text-primary-content m-4">
                             <div className="card-body">
-                                <h2 className="card-title">{capsule.status}</h2>
-                                <p>{capsule.capsule_id}</p>
-                                <p>{capsule.details}</p>
+                                <h2 className="card-title">Status: {capsule.status}</h2>
+                                <p>Serial: {capsule.capsule_serial}</p>
+                                <p>Id: {capsule.capsule_id}</p>
+                                <p>Description: {capsule.details}</p>
                                 <div className="card-actions justify-end">
-                                    <button className="btn">Details</button>
+                                    <button className="btn btn-success">Details</button>
                                 </div>
                             </div>
                         </div>
